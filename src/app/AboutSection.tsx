6cbbf8b5aecfa4e66e383a007b02c30a6fac6b8d
Rrazmik <robertrecalo@icloud.com>
2023-11-02 20:33:38 -0400

    Add AboutSection.tsx for home page

---
+++ src/app/AboutSection.tsx
@@ -0,0 +1,63 @@
+import { AnimatePresence, Variants, motion } from 'framer-motion'
+import React, { useEffect, useState } from 'react'
+
+interface AboutSectionProps {
+    selection : number
+}
+
+export default function AboutSection({selection} : AboutSectionProps) {
+
+    const [animated, setAnimated] = useState<boolean>(false);
+
+    const sectionVariants : Variants = {
+        initial:{
+            x:"-100vw",
+            opacity:0,
+        },
+        animate:{
+            x:"0vw",
+            opacity:1,
+            transition:{
+                duration:0.75,
+                delay: animated ? 0 : 1.5
+            }
+        },
+        exit:{
+            x:"100vw",
+            opacity:0,
+            transition:{
+                duration:0.75,
+            }
+        }
+    }
+    
+    useEffect(()=>{
+
+    }, [selection])
+
+  return (
+    <div className='mt-3 text-zinc-400 text-[0.75rem] sm:text-sm'>
+        <AnimatePresence mode='popLayout'>
+    {selection === 0 ?
+    <motion.div key="software" className='' variants={sectionVariants} initial="initial" animate="animate" exit="exit" onAnimationComplete={()=>{setAnimated(true)}}>
+        {"With several years of programming experience in various languages and development environments, I've implemented"
+        +" software solutions in some of the latest technologies."}
+        <p className='mt-2'>I firmly believe tools matter less than the person behind them.</p>
+    </motion.div>
+    :
+    selection === 1 ? 
+    <motion.div key="music" className='' variants={sectionVariants} initial="initial" animate="animate" exit="exit">
+        {"I play Piano, Acoustic Guitar, and the Bass. Having learnt three instruments, I've built discipline through long hours of practice."}
+        <p className='mt-2'>{"I know what it's like to find my way around frustration until I reach a breakthrough."}</p>
+        
+    </motion.div>
+    :
+    <motion.div key="leadership" className='' variants={sectionVariants} initial="initial" animate="animate" exit="exit">
+        {"Closely related to my musicianship, I've had the honor to help lead my local church's worship team."}
+        <p className='mt-2'>{"People are not easy, but practicing servant leadership has help me grow immensely."}</p>
+    </motion.div>
+    }
+        </AnimatePresence>
+    </div>
+  )
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,12 @@
 'use client'
-import {motion, useScroll, useAnimationControls, Variants} from 'framer-motion'
-import { useRouter } from 'next/navigation';
+import { AnimatePresence, motion,  Variants, } from 'framer-motion'
 import LinkButton from './components/LinkButton';
+import { useState } from 'react';
+import AboutSection from './AboutSection';
 
 export default function Home() {
 
-  const { scrollYProgress } = useScroll();
-  const controls = useAnimationControls();
-  const router = useRouter();
+  const [selection, setSelection] = useState<number>(0);
 
   const headerVariant : Variants = {
     initial:{
@@ -32,7 +31,19 @@ export default function Home() {
     animate:{
       opacity:1,
       y:0,
-      transition:{duration:0.5}
+      transition:{duration:0.5, staggerChildren:0.25}
+    }
+  }
+
+  const traitVariant : Variants = {
+    initial:{
+      opacity:0,
+      y:-5
+    },
+    animate:{
+      opacity:1,
+      y:0,
+      transition:{duration:0.25}
     }
   }
 
@@ -41,16 +52,31 @@ export default function Home() {
   return (
     <motion.div className='w-full text-slate-300 h-[300vh] mt-2 sm:mt-[52px] p-5' >
       <motion.div id="hero" layout="size" className='text-4xl sm:text-6xl mt-0 sm:mt-10 origin-left whitespace-nowrap' variants={headerVariant} initial="initial" animate="animate">
-        Software Engineer
-        <motion.div className='text-xs sm:text-base font-light mt-2 whitespace-normal' variants={subHeaderVariant}>
-        Eager new graduate with experience in the most popular technologies
+        {"Hi, I'm Robert"}
+       <motion.div className='text-xs sm:text-base font-light mt-2 whitespace-normal' variants={subHeaderVariant}>
+          <motion.p className={`inline-block`} variants={traitVariant} 
+          animate={{color: selection === 0 ? 'rgb(59, 130, 246)' : 'rgb(255, 255, 255)'}}
+          onClick={()=>{setSelection(0)}}>
+            {"Software Engineer"}
+          </motion.p>
+          <motion.p className={`inline-block`} variants={traitVariant} onClick={()=>{setSelection(1)}}
+          animate={{color: selection === 1 ? 'rgb(59, 130, 246)' : 'rgb(255, 255, 255)'}}>
+          <span className='text-white mx-1'>/</span>{"Musician"}
+          </motion.p>
+          <motion.p className={`inline-block`} variants={traitVariant} onClick={()=>{setSelection(2)}}
+          animate={{color: selection === 2 ? 'rgb(59, 130, 246)' : 'rgb(255, 255, 255)'}}>
+          <span className='text-white mx-1'>/</span>{"Leader"}
+          </motion.p>
+          <motion.p className='text-xs inline-block ml-3'
+          initial={{opacity:0, x:10}}
+          animate={{opacity:[1, 1, 1], x:[0, 10, 0], transition:{delay:3, repeat:Infinity, repeatDelay:3, repeatType:"mirror"}}}>{"<- click me"}</motion.p>
         </motion.div>
       </motion.div>
-      <div className='flex flex-col sm:flex-row mt-20 gap-10'>
+      <AboutSection selection={selection}/>
+      <motion.div layout="position" className='flex flex-col sm:flex-row items-center justify-center mt-20 gap-10'>
       <LinkButton buttonName='Projects' buttonDesc="See some of the projects I've worked on" hrefLink='/projects'/>
       <LinkButton buttonName='Techs' buttonDesc='Have a quick look at the technologies I know' hrefLink='/techs'/>
-      </div>
-
+      </motion.div>
     </motion.div>
   )
 }
