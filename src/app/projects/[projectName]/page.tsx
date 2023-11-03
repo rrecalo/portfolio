cd91f153caf57ab13deccd73f315d2c8e1b9c0de
Rrazmik <robertrecalo@icloud.com>
2023-11-03 19:15:19 -0400

    Add return arrow/button to project view

--- src/app/projects/[projectName]/page.tsx
+++ src/app/projects/[projectName]/page.tsx
@@ -1,16 +1,32 @@
 'use client'
 import {useEffect} from 'react'
-import { useParams } from "next/navigation";
+import {motion} from 'framer-motion'
+import { useParams, useRouter } from "next/navigation";
 
 export default function Page() {
   const params = useParams();
-  
+  const router = useRouter();
+
   useEffect(()=>{
     console.log(params);
   },[params])
 
+  function returnToProjects(){
+    router.push("/projects");
+  }
+
   return (
-    <div className='text-xl font-bold sm:mt-[72px]'>{params?.projectName}</div>
+    <motion.div className='text-xl font-bold sm:mt-[72px]  p-5 rounded-lg bg-zinc-950'
+    initial={{opacity:0}} animate={{opacity:1}} exit={{opacity:0}}
+    >
+      <div className='flex justify-between items-center'>
+      {params?.projectName}
+      <motion.div className='text-zinc-400 mr-1 py-1 rounded-lg px-2'
+      animate={{backgroundColor:"transparent"}} whileHover={{backgroundColor:"rgb(24, 24, 27)"}} onClick={()=>returnToProjects()}>
+        {'<-'}
+      </motion.div>
+      </div>
+    </motion.div>
   )
 }
 
