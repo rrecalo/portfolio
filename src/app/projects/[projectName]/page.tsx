44d17d4117a30d81ff2fc61681824d87e4b6211f
Rrazmik <robertrecalo@icloud.com>
2023-11-07 18:21:38 -0500

    Apply text color on elements to prevent issue on mobile devices for certain texts

--- src/app/projects/[projectName]/page.tsx
+++ src/app/projects/[projectName]/page.tsx
@@ -25,7 +25,7 @@ export default function Page() {
     <motion.div className='relative text-xl h-60 mt-[72px] mx-5 sm:mx-auto p-5 rounded-lg bg-zinc-950'
     initial={{opacity:0}} animate={{opacity:1}} exit={{opacity:0}}
     >
-      <div className='flex justify-between items-center font-bold'>
+      <div className='flex justify-between items-center font-bold text-zinc-50'>
         {params?.projectName}
         <motion.div className='text-zinc-400 mr-1 py-1 rounded-lg px-2'
         animate={{backgroundColor:"transparent"}} whileHover={{backgroundColor:"rgb(24, 24, 27)"}} onClick={()=>returnToProjects()}>
@@ -36,13 +36,13 @@ export default function Page() {
         {projectData[0]?.summary}
       </motion.p>
       <div className='absolute bottom-5 left-5 flex gap-5'>
-        <motion.a target="_blank" href={projectData[0]?.repo} className=' px-3 py-2 text-sm  bg-black rounded-lg'
+        <motion.a target="_blank" href={projectData[0]?.repo} className='px-3 py-2 text-sm text-zinc-100  bg-black rounded-lg'
         initial={{y:-10, opacity:0}}
         animate={{opacity:1, y:0}}
         whileHover={{x:3, boxShadow: "2px 2px 0px 0px rgb(37, 99, 235, 0.5)", transition:{ease: "easeInOut"}}}>
           View on GitHub
         </motion.a>
-        <motion.a target="_blank" href={projectData[0]?.deploy} className='px-3 py-2 text-sm  bg-black rounded-lg'
+        <motion.a target="_blank" href={projectData[0]?.deploy} className='px-3 py-2 text-sm text-zinc-100 bg-black rounded-lg'
         initial={{y:-10, opacity:0}}
         animate={{opacity:1, y:0}}
         whileHover={{x:3, boxShadow: "2px 2px 0px 0px rgb(37, 99, 235, 0.5)", transition:{ease: "easeInOut"}}}>
