89cece2da0bbe8a8ef8db1b7545ea3f03f81c653
Rrazmik <robertrecalo@icloud.com>
2023-11-06 23:10:18 -0500

    Add more hover animation response to ProjectCards

--- src/app/projects/ProjectCard.tsx
+++ src/app/projects/ProjectCard.tsx
@@ -10,6 +10,7 @@ interface ProjectCardProps {
 export default function ProjectCard({title, desc} : ProjectCardProps) {
 
   const [max, setMax] = useState<boolean>(false);
+  const [hover, setHover] = useState<boolean>(false);
   const router = useRouter();
 
   function openProject(){
@@ -17,16 +18,28 @@ export default function ProjectCard({title, desc} : ProjectCardProps) {
   }
 
   return (
-    <motion.div className='top-[72px] left-0 h-60 bg-zinc-950 p-5 rounded-lg'
+    <motion.div className='relative top-[72px] left-0 h-40 bg-zinc-950 p-5 rounded-lg'
     initial={{opacity:0}} animate={{opacity:1, width: "100%"}} 
+    onMouseEnter={()=>{setHover(true)}}
+    onMouseLeave={()=>{setHover(false)}}
     whileHover={{x:10, boxShadow: "4px 4px 0px 0px rgb(37, 99, 235, 0.5)", transition:{ease: "easeInOut"}}}
     onClick={()=>{openProject()}}>
         <div className='text-xl font-bold text-zinc-50'>
             {title}
         </div>
-        <p className='text-sm text-zinc-300'>
+        <p className='text-sm text-zinc-400'>
             {desc}
         </p>
+        <motion.div className="absolute bottom-5 right-5">
+          <motion.span className='text-base mr-2 text-zinc-300'
+          initial={{opacity:0, y:5}} animate={hover ? {opacity:1, y:0} : {}}>
+            see details
+          </motion.span>
+          <motion.span className=''
+          initial={{y:5}} animate={hover ? {y:0, color: "rgb(244, 244, 245)"} : {color:"rgb(161, 161, 170)"}}>
+          {"->"}
+          </motion.span>
+        </motion.div>
     </motion.div>
   )
 }
