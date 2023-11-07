f96e4eea87af52a42183ae132d0f491097d7fbfe
Rrazmik <robertrecalo@icloud.com>
2023-11-06 23:46:53 -0500

    Add data.tsx to provide info for projects (to be expanded)

---
+++ src/app/data.tsx
@@ -0,0 +1,18 @@
+export const data = [
+    {
+        name:"ONote",
+        subtitle:"A minimal WYSIWYG note-taking web application.",
+        summary:"A single-page web application for creating, editing, storing, and organizing rich-text note documents. Equipped with note organization features including drag-n-drop and collapsible folders. Built on the MERN stack with robust user authentication powered by Auth0.",
+        repo:"https://github.com/rrecalo/ONote",
+        deploy:"https://onote.cloud/app",
+
+    },
+    {
+        name:"Temperature-Monitor",
+        subtitle:"An IoT project powered by AWS services.",
+        summary:"A web-based temperature/humidity monitoring dashboard that displays the latest readings from an Espressif ESP32 microcontroller running embedded C++ code to interface with a DHT11 sensor.",
+        repo:"https://github.com/rrecalo/temperature-monitor",
+        deploy:"https://master.d3tgy8y41907fm.amplifyapp.com/",
+
+    }
+]
--- src/app/projects/[projectName]/page.tsx
+++ src/app/projects/[projectName]/page.tsx
@@ -1,30 +1,53 @@
 'use client'
-import {useEffect} from 'react'
+import {useEffect, useState} from 'react'
 import {motion} from 'framer-motion'
 import { useParams, useRouter } from "next/navigation";
+import { data } from '@/app/data';
 
 export default function Page() {
+
+  const [projectData, setProjectData] = useState<any[]>([]);
   const params = useParams();
   const router = useRouter();
 
   useEffect(()=>{
-    console.log(params);
+    if(params?.projectName){
+      setProjectData(data.filter((obj : any) => obj.name === params.projectName));
+    }
   },[params])
 
   function returnToProjects(){
     router.push("/projects");
   }
 
+
   return (
-    <motion.div className='text-xl font-bold mt-[72px] mx-5 sm:mx-auto p-5 rounded-lg bg-zinc-950'
+    <motion.div className='relative text-xl h-60 mt-[72px] mx-5 sm:mx-auto p-5 rounded-lg bg-zinc-950'
     initial={{opacity:0}} animate={{opacity:1}} exit={{opacity:0}}
     >
-      <div className='flex justify-between items-center'>
-      {params?.projectName}
-      <motion.div className='text-zinc-400 mr-1 py-1 rounded-lg px-2'
-      animate={{backgroundColor:"transparent"}} whileHover={{backgroundColor:"rgb(24, 24, 27)"}} onClick={()=>returnToProjects()}>
-        {'<-'}
-      </motion.div>
+      <div className='flex justify-between items-center font-bold'>
+        {params?.projectName}
+        <motion.div className='text-zinc-400 mr-1 py-1 rounded-lg px-2'
+        animate={{backgroundColor:"transparent"}} whileHover={{backgroundColor:"rgb(24, 24, 27)"}} onClick={()=>returnToProjects()}>
+          {'<-'}
+        </motion.div>
+      </div>
+      <motion.p className='text-zinc-400 text-sm font-normal'>
+        {projectData[0]?.summary}
+      </motion.p>
+      <div className='absolute bottom-5 left-5 flex gap-5'>
+        <motion.a target="_blank" href={projectData[0]?.repo} className=' px-3 py-2 text-sm  bg-black rounded-lg'
+        initial={{y:-10, opacity:0}}
+        animate={{opacity:1, y:0}}
+        whileHover={{x:3, boxShadow: "2px 2px 0px 0px rgb(37, 99, 235, 0.5)", transition:{ease: "easeInOut"}}}>
+          View on GitHub
+        </motion.a>
+        <motion.a target="_blank" href={projectData[0]?.repo} className='px-3 py-2 text-sm  bg-black rounded-lg'
+        initial={{y:-10, opacity:0}}
+        animate={{opacity:1, y:0}}
+        whileHover={{x:3, boxShadow: "2px 2px 0px 0px rgb(37, 99, 235, 0.5)", transition:{ease: "easeInOut"}}}>
+          Open Live Deploy
+        </motion.a>
       </div>
     </motion.div>
   )
