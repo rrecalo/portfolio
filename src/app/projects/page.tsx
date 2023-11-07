690d4535b8c426d9b65619a1065a19e79c809c27
Rrazmik <robertrecalo@icloud.com>
2023-11-06 23:49:38 -0500

    Now dynamically generate projectcards on project page based off entries in data.tsx

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -2,13 +2,13 @@
 import React from 'react'
 import {motion} from 'framer-motion'
 import ProjectCard from './ProjectCard'
+import { data } from '../data'
 
 export default function Projects() {
   return (
     <motion.div key="projects" className=' text-slate-300 h-[300vh] mt-[72px] sm:mt-[72px] mx-5' initial={{opacity:0}} animate={{opacity:1}} exit={{opacity:0}}>
       <motion.div className='flex flex-col mt-0 sm:mt-10 gap-10'>
-        <ProjectCard title='ONote' desc="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam eget cursus nisl. Donec lobortis sit amet urna non porttitor. Aliquam ornare suscipit aliquet. In elementum libero id lectus auctor rhoncus."/>
-        <ProjectCard title='Temperature-Monitor' desc="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam eget cursus nisl. Donec lobortis sit amet urna non porttitor. Aliquam ornare suscipit aliquet. In elementum libero id lectus auctor rhoncus."/>
+        {data.map((project : any)=>  <ProjectCard key={project.name} title={project.name} desc={project.summary}/>)}
       </motion.div>
     </motion.div>
   )
