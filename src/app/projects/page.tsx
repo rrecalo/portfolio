2cc4ab30f17f466be2db32a90841b538d8c3d36d
Rrazmik <robertrecalo@icloud.com>
2023-11-07 23:03:14 -0500

    Add headers for small displays on /projects and /techs pages

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -3,10 +3,14 @@ import React from 'react'
 import {motion} from 'framer-motion'
 import ProjectCard from './ProjectCard'
 import { data } from '../data'
+import {headerVariant} from '../page'
 
 export default function Projects() {
   return (
     <motion.div key="projects" className='text-slate-300 h-[300vh] mt-2 p-5 sm:mt-[72px]' initial={{opacity:0}} animate={{opacity:1}} exit={{opacity:0}}>
+      <motion.div  layout="size" className='text-4xl sm:text-6xl sm:hidden origin-left whitespace-nowrap' variants={headerVariant} initial="initial" animate="animate">
+        {"My Projects"}
+      </motion.div>
       <motion.div className='flex flex-col mt-0 sm:mt-10 gap-10'>
         {data.map((project : any)=>  <ProjectCard key={project.name} title={project.name} desc={project.subtitle} tags={project.tags}/>)}
       </motion.div>
--- src/app/techs/page.tsx
+++ src/app/techs/page.tsx
@@ -1,11 +1,14 @@
 'use client'
 import React from 'react'
 import {motion} from 'framer-motion'
+import { headerVariant } from '../page'
 
 export default function Techs() {
   return (
-    <motion.div key="techs" className='w-full text-slate-300 h-[300vh] mt-2 sm:mt-[52px] p-5' initial={{opacity:0}} animate={{opacity:1}} exit={{opacity:0}}>
-      techs
+    <motion.div key="techs" className='text-slate-300 h-[300vh] mt-2 p-5 sm:mt-[72px]' initial={{opacity:0}} animate={{opacity:1}} exit={{opacity:0}}>
+      <motion.div  layout="size" className='text-4xl sm:text-6xl sm:hidden origin-left whitespace-nowrap' variants={headerVariant} initial="initial" animate="animate">
+        {"My Techs"}
+      </motion.div>
     </motion.div>
   )
 }
