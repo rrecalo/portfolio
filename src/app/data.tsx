ebca8ca5fee4d100b12a4e6ea3c5767b843ccd00
Robert Recalo <rrecalo3@ggc.edu>
2023-11-27 23:38:09 -0500

    Adjust tag count in project data to be more concise and prevent overflows on smaller displays

--- src/app/data.tsx
+++ src/app/data.tsx
@@ -2,7 +2,7 @@ export const data = [
     {
         name:"ONote",
         subtitle:"A cloud-based WYSIWYG note-taking web application.",
-        tags: ["ReactJS", "rust (tokio/axum)", "MongoDB", "auth0", "framer-motion", "tailwind-css"],
+        tags: ["reactJS", "rust", "mongoDB", "auth0"],
         summary:"A single-page web application for creating, editing, storing, and organizing rich-text note documents. Equipped with note organization features including drag-n-drop and collapsible folders. Initially built n the MERN stack with robust user authentication powered by Auth0. I recently rebuilt the backend server in Rust - now it's blazingly fast!",
         repo:"https://github.com/rrecalo/ONote",
         deploy:"https://onote.cloud/app",
@@ -10,7 +10,7 @@ export const data = [
     {
         name:"Temperature-Monitor",
         subtitle:"An IoT project powered by AWS services.",
-        tags: ["AWS (IoT, DynamoDB)", "C++ (Embedded)", "ReactJS", , "Chart.js"],
+        tags: ["aws", "c++", "reactJS", "chart.js"],
         summary:"A web-based temperature/humidity monitoring dashboard that displays the latest readings from an Espressif ESP32 microcontroller running embedded C++ code to interface with a DHT11 sensor.",
         repo:"https://github.com/rrecalo/temperature-monitor",
         deploy:"https://master.d3tgy8y41907fm.amplifyapp.com/",
@@ -18,7 +18,7 @@ export const data = [
     {
         name:"Crypto-Betting-App",
         subtitle:"A scrappy capstone-project web-app built on CoinGecko's API",
-        tags: ["MERN", "auth0", "CoinGecko","tailwind-css"],
+        tags: ["MERN", "auth0", "Rest API","tailwind-css"],
         summary:"A capstone project built iteratively in a team of three following Agile principles during my last semester of university. Reads cryptocurrency price data from CoinGecko's free API to resolve bets placed by users. Built in the MERN stack with love.",
         repo:"https://github.com/BenMoreira/crypto-betting-app",
         deploy:"https://crypto-betting-app.netlify.app/",
@@ -26,7 +26,7 @@ export const data = [
     {
         name:"Portfolio",
         subtitle:"The app you're currently using, isn't it great?",
-        tags: ["NextJS", "responsive", "framer-motion", "tailwind-css"],
+        tags: ["nextJS", "framer-motion", "tailwind-css"],
         summary:"A NextJS app showcasing my personality, projects, and abilities. Fully equipped with snazzy animations and a sleek colorway.",
         repo:"https://github.com/rrecalo/portfolio",
         deploy:"https://www.robertrecalo.com",
